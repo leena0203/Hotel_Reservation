6bd85ade1090e180dc3cf84cf1a463422204f80d
leena0203 <leenasarode02@gmail.com>
2020-10-16 01:04:08 +0530

    Added: Method to return cheap hotel name given weekday and weekend rates

--- src/main/java/com/HotelReservation/HotelReservation.java
+++ src/main/java/com/HotelReservation/HotelReservation.java
@@ -1,5 +1,6 @@
 package com.HotelReservation;
-
+import java.time.temporal.ChronoField;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
@@ -16,7 +17,6 @@ public class HotelReservation {
 	public static void addHotel(String name,int weekdayRate) {
 		Hotel H1 = new Hotel(name,weekdayRate);
 		hotelList.add(H1);
-		
 	}
 	/**
 	 * UC3_Add hotel name and week rate rate and weekendRate
@@ -24,10 +24,7 @@ public class HotelReservation {
 	public static void addHotel(String name,int weekdayRate,int weekendRate) {
 		Hotel H1 = new Hotel(name,weekdayRate, weekendRate);
 		hotelList.add(H1);
-		
 	}
-	
-
 	/**
 	 * String to local date format
 	 * @param date
@@ -39,28 +36,60 @@ public class HotelReservation {
 		LocalDate localDate = LocalDate.parse(value, formatter);
 		return localDate;
 	}
+	
+	/**
+	 * method to find weekdays and weekends
+	 * @param arrival
+	 * @param departure
+	 * @return
+	 */
+	public static int[] numberOfDays(String arrival, String departure) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy");
+		// convert String to LocalDate
+		LocalDate from = LocalDate.parse(arrival, formatter);
+		LocalDate to = LocalDate.parse(departure, formatter);
+		int numOfweekdays = 0;
+		int numOfWeekends = 0;
+		for (LocalDate date = from; date.isBefore(to.plusDays(1)); date = date.plusDays(1)) {
+			DayOfWeek day = DayOfWeek.of(date.get(ChronoField.DAY_OF_WEEK)); // returns enum of day
+			switch (day) {
+			case SATURDAY:
+				numOfWeekends++;
+				break;
+			case SUNDAY:
+				numOfWeekends++;
+				break;
+			default:
+				numOfweekdays++;
+				break;
+			}
+		}
+		return new int[] { numOfweekdays, numOfWeekends };
+	}
 
 	/**
 	 * UC2_Cheapest hotel
+	 * UC4_Refactored to find cheapest hotel with weekends rate
 	 * @param departure 
 	 * @param arrival 
 	 * @return 
 	 */
 
-	public  String cheapestHotel(String arrival, String departure) {
-//		Scanner sc = new Scanner(System.in);
-//		System.out.println("Enter Arrival Date: ");
-//		String arrival = sc.nextLine();
-//		System.out.println("Enter Departure Date: ");
-//		String departure = sc.nextLine();
+	public static  String cheapestHotel(String arrival, String departure) {
+		//		Scanner sc = new Scanner(System.in);
+		//		System.out.println("Enter Arrival Date: ");
+		//		String arrival = sc.nextLine();
+		//		System.out.println("Enter Departure Date: ");
+		//		String departure = sc.nextLine();
 		LocalDate localDate1 = stringToLocalDate(arrival);
 		LocalDate localDate2 = stringToLocalDate(departure);
-		// calculate difference
-		int days = (int)Period.between(localDate1, localDate2).getDays();
+		int[] numOfDays = numberOfDays(arrival, departure);
 		int cost = Integer.MAX_VALUE;
 		String cheapHotel = "";
 		for (Hotel hotel: hotelList ) {
-			int newCost = hotel.weekdayRate*days ;
+			int newCost1 = hotel.weekdayRate*numOfDays[0] ;
+			int newcost2 = hotel.weekendRate*numOfDays[1];
+			int newCost = newCost1 + newcost2;
 			if (newCost < cost) {
 				cheapHotel = hotel.name;
 				cost = newCost;
@@ -68,16 +97,16 @@ public class HotelReservation {
 		}
 		System.out.println("Cheapest hotel : " + cheapHotel + ",  Rates : $" + cost);
 		return cheapHotel;
-		
+
 	}
 	public int size() {
 		return hotelList.size() ;
 	}
 
-//	public static void main(String[] args) {
-//		System.out.println("Welcome to Hotel Reservation Program");
-//		addHotel();
-//		//cheapestHotel("10 Sep 2020","11 Sep 2020");
-//	}
+	//	public static void main(String[] args) {
+	//		System.out.println("Welcome to Hotel Reservation Program");
+	//		addHotel();
+	//		cheapestHotel();
+	//	}
 }
 
--- src/test/java/com/HotelReservation/HotelReservationTest.java
+++ src/test/java/com/HotelReservation/HotelReservationTest.java
@@ -11,29 +11,43 @@ class HotelReservationTest {
 	 * UC2_Return cheapest hotel name
 	 */
 	@Test
-	void cheapestMethod_shouldReturn_hotelNameAndToatalRate() {
+	void cheapestMethod_shouldReturn_hotelName() {
 		HotelReservation test = new HotelReservation();
 		test.addHotel("Lakewood", 110);
 		test.addHotel("Bridgewood",160);
 		test.addHotel("Ridgewood",220);
 		String result = test.cheapestHotel("10 Sep 2020", "11 Sep 2020");
 		assertEquals("Lakewood",result);
+	}
+	/**
+	 * UC4_Return cheapest hotel name
+	 */
+	@Test
+	void givenDateAndRateOfStay_shouldReturn_cheapesthotelName() {
+		HotelReservation test = new HotelReservation();
+		//test.addHotel("Lakewood", 110,90);
+		test.addHotel("Bridgewood",150,50);
+		test.addHotel("Ridgewood",220,150);
+		String result = test.cheapestHotel("11 Sep 2020", "12 Sep 2020");
+		assertEquals("Bridgewood",result);
 
 	}
 	
-	/**
-	 * UC3_Add hotel name and weekday and weekend rates
-	 */
-	@Test
-	public void givenHotelNameAndWeekendRate_shouldReturn_listSize() {
-		
-		HotelReservation test = new HotelReservation();
-		test.addHotel("Lakewood", 110,90);
-		test.addHotel("Bridgewood",150,50);
-		test.addHotel("Ridgewood",220,150);
-		int result = test.hotelList.size();
-		assertEquals(3, result);
-	}
+	
+//	/**
+//	 * UC3_Add hotel name and weekday and weekend rates
+//	 */
+//	@Test
+//	public void givenHotelNameAndWeekendRate_shouldReturn_listSize() {
+//		
+//		HotelReservation test = new HotelReservation();
+//		test.addHotel("Lakewood", 110,90);
+//		test.addHotel("Bridgewood",150,50);
+//		test.addHotel("Ridgewood",220,150);
+//		int result = test.hotelList.size();
+//		assertEquals(3, result);
+//	}
+	
 	
 	
 
