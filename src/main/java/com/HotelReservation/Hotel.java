9965da4249c97b3419c2dcff0f951f93430513d5
leena0203 <leenasarode02@gmail.com>
2020-10-15 12:22:53 +0530

    Added: Method to add Hotel name and rates

---
+++ src/main/java/com/HotelReservation/Hotel.java
@@ -0,0 +1,31 @@
+package com.HotelReservation;
+
+public class Hotel {
+	String name;
+	int weekdayRate, weekendRate;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getWeekdayRate() {
+		return weekdayRate;
+	}
+	public void setWeekdayRate(int weekdayRate) {
+		this.weekdayRate = weekdayRate;
+	}
+	public int getWeekendRate() {
+		return weekendRate;
+	}
+	public void setWeekendRate(int weekendRate) {
+		this.weekendRate = weekendRate;
+	}
+	public Hotel(String name, int weekdayRate, int weekendRate) {
+		this.name = name;
+		this.weekdayRate = weekdayRate;
+		this.weekendRate = weekendRate;
+
+	}
+}
+
--- src/main/java/com/HotelReservation/HotelReservation.java
+++ src/main/java/com/HotelReservation/HotelReservation.java
@@ -1,8 +1,18 @@
 package com.HotelReservation;
 
 public class HotelReservation {
-	
+/**
+ * UC1_Add hotel name
+ */
+	public static void addHotel() {
+		Hotel H1 = new Hotel("Lakewood",110,90);
+		Hotel H2 = new Hotel("Bridgewood",160,60);
+		Hotel H3 = new Hotel("Ridgewood",220,150);
+	}
+
 	public static void main(String[] args) {
+		HotelReservation hotelReservation = new HotelReservation();
 		System.out.println("Welcome to Hotel Reservation Program");
+		addHotel();
 	}
 }
