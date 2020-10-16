b855576dbb470242fb850141b51e5dc58bd991c2
leena0203 <leenasarode02@gmail.com>
2020-10-16 21:33:45 +0530

    Added: Rating of hotel

--- src/test/java/com/HotelReservation/HotelReservationTest.java
+++ src/test/java/com/HotelReservation/HotelReservationTest.java
@@ -22,21 +22,19 @@ class HotelReservationTest {
 	//		String result = test.findCheapestHotel("10 Sep 2020", "11 Sep 2020");
 	//		assertEquals("Lakewood",result);
 	//	}
-		/**
-		 * UC4_Return cheapest hotel name
-		 */
-		@Test
-		void givenDateAndRateOfStay_shouldReturn_cheapesthotelName() {
-			HotelReservation test = new HotelReservation();
-			test.addHotel("Lakewood", 110,90);
-			test.addHotel("Bridgewood",150,50);
-			test.addHotel("Ridgewood",220,150);
-			List<Hotel> result = test.minimumRate("11 Sep 2020", "12 Sep 2020");
-			assertEquals("Bridgewood",result.get(0).getName());
-			assertEquals("Lakewood",result.get(1).getName());
-			
-	
-		}
+	//		/**
+	//		 * UC4_Return cheapest hotel name
+	//		 */
+	//		@Test
+	//		void givenDateAndRateOfStay_shouldReturn_cheapesthotelName() {
+	//			HotelReservation test = new HotelReservation();
+	//			test.addHotel("Lakewood", 110,90);
+	//			test.addHotel("Bridgewood",150,50);
+	//			test.addHotel("Ridgewood",220,150);
+	//			List<Hotel> result = test.minimumRate("11 Sep 2020", "12 Sep 2020");
+	//			assertEquals("Bridgewood",result.get(0).getName());
+	//			assertEquals("Lakewood",result.get(1).getName());
+	//		}
 	//	
 	//	/**
 	//	 * UC3_Add hotel name and weekday and weekend rates
@@ -52,19 +50,19 @@ class HotelReservationTest {
 	//		assertEquals(3, result);
 	//	}
 
-	//	/**
-	//	 * UC5_Add hotel name and weekday and weekend rates and rating
-	//	 */
-	//	@Test
-	//	public void givenHotelNameAndWeekendRateAndRating_shouldReturn_listSize() {
-	//
-	//		HotelReservation test = new HotelReservation();
-	//		test.addHotel("Lakewood", 110,90, 3);
-	//		test.addHotel("Bridgewood",150,50, 4);
-	//		test.addHotel("Ridgewood",220,150, 5);
-	//		int result = test.hotelList.size();
-	//		assertEquals(3, result);
-	//	}
+		/**
+		 * UC5_Add hotel name and weekday and weekend rates and rating
+		 */
+		@Test
+		public void givenHotelNameAndWeekendRateAndRating_shouldReturn_listSize() {
+	
+			HotelReservation test = new HotelReservation();
+			test.addHotel("Lakewood", 110,90, 3);
+			test.addHotel("Bridgewood",150,50, 4);
+			test.addHotel("Ridgewood",220,150, 5);
+			int result = test.hotelList.size();
+			assertEquals(3, result);
+		}
 
 
 
